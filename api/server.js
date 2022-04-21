46e0bb5e8750808e181a4c2ffdc8da205604f800
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 11:56:17 -0700

    server created

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const recipesRouter = require('./recipes/router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/recipes', recipesRouter);
+
+module.exports = server;
