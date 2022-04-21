09ee8ed7c038f1efa75b3b43a574cb340f5eae87
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 13:35:01 -0700

    config set up

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,44 @@
-const sharedConfig = {
-    client: 'sqlite3',
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
-    useNullAsDefault: true,
-    pool: {
-        afterCreate: (conn, done) => {
-            conn.run('PRAGMA foreign_key = ON', done);
-        }
-    }
-}
+// const sharedConfig = {
+//     client: 'sqlite3',
+//     migrations: { directory: './data/migrations' },
+//     seeds: { directory: './data/seeds' },
+//     useNullAsDefault: true,
+//     pool: {
+//         afterCreate: (conn, done) => {
+//             conn.run('PRAGMA foreign_key = ON', done);
+//         }
+//     }
+// }
+
+// module.exports = {
+//     development: {
+//         ...sharedConfig,
+//         connection: { filename: './data/cook_book.db3' },
+//     },
+//     testing: {
+//         ...sharedConfig,
+//         connection: { filename: './data/cook_book.test.db3' },
+//     },
+//     production: {}
+// }
 
 module.exports = {
     development: {
-        ...sharedConfig,
-        connection: { filename: './data/cook_book.db3' },
+      client: 'sqlite3',
+      useNullAsDefault: true, // needed for sqlite
+      connection: {
+        filename: './data/cook_book.db3',
+      },
+      migrations: {
+        directory: './data/migrations'
+      },
+      seeds: {
+        directory: './data/seeds'
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run('PRAGMA foreign_key = ON', done);
+        }
+      }
     },
-    testing: {
-        ...sharedConfig,
-        connection: { filename: './data/cook_book.test.db3' },
-    },
-    production: {}
-}
+  };
