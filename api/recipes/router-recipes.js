9a6d39fb0be94bcf9b25654a19a5c6ae35b52694
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 13:07:09 -0700

    knexfile and db-config created

--- api/recipes/router-recipes.js
+++ api/recipes/router-recipes.js
@@ -3,9 +3,14 @@ const Recipes = require('./model-recipes')
 
 const router = express.Router();
 
-router.use('*', (req, res) => {
-    res.json({ api: 'up' })
-})
+router.get('/:recipe_id', (req, res, next) => {
+    Recipes.getRecipeById(req.params.recipe_id)
+        .then(resource => {
+            res.status(200).json(resource);
+        })
+        .catch(next);
+  });
+
 
 router.use((err, req, res, next) => {
     res.status(500).json({ message: 'error', message: err.message, stack: err.stack })
--- api/server.js
+++ api/server.js
@@ -6,4 +6,8 @@ const server = express();
 server.use(express.json());
 server.use('/api/recipes', recipesRouter);
 
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
 module.exports = server;
